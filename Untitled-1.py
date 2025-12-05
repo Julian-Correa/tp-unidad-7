e27a12614aa17a83fe3e460514bd06fe083cb523
JulianCorrea <108768144+Julian-Correa@users.noreply.github.com>
2025-12-05 15:52:04 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,36 @@
+#ejercicio 1
+
+for n in range(0,101,1):
+    print(n)
+
+
+#ejercicio 2
+
+numero = int(input("ingresar numero entero = "))
+
+contarDigitos = len(str(numero))
+print(f"la cantidad de digitos es = {contarDigitos}")
+
+
+#ejercicio 3
+
+valor1 = int(input("ingrese el primer numero = "))
+valor2 = int(input("ingrese el segundo valor = "))
+suma = 0
+for numeros in range(valor1 + 1,valor2,1):
+    suma += numeros
+    print(suma)
+    
+#ejercicio 4
+# repetir el input al cliente a menos que 
+# indique 0.
+
+
+
+while numero2 != 0:
+    numero2 = int(input("ingrese numero entero = ")) 
+    sumar =+ numero2
+    
+print(sumar)
+    
+
